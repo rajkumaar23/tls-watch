e96d1ab99f9fdb9591dbb216f4bdb376eb374d29
Rajkumar <rajkumaar2304@icloud.com>
2023-12-25 16:22:10 -0700

    read config from env instead of json

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -19,13 +23,7 @@ var WaitGroup sync.WaitGroup
 var HttpClient = &http.Client{}
 
 func main() {
-	configFileContents, err := os.ReadFile("config.json")
-	if err != nil {
-		panic(err)
-	}
-	if err = json.Unmarshal(configFileContents, &config); err != nil {
-		panic(err)
-	}
+	loadConfigFromEnv()
 
 	for _, hostname := range config.Hostnames {
 		WaitGroup.Add(1)
@@ -33,4 +31,41 @@ func main() {
 	}
 
 	WaitGroup.Wait()
+}
+
+func loadConfigFromEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("error loading .env file: %s\nproceeding to see if env variables are set already", err)
+	}
+
+	hostnames := strings.TrimSpace(os.Getenv("HOSTNAMES"))
+	if hostnames == "" {
+		log.Panicf("hostnames are empty")
+	}
+
+	config.Hostnames = strings.Split(hostnames, ",")
+	if len(config.Hostnames) < 1 {
+		log.Fatalf("failed parsing hostnames: %+v\n", hostnames)
+	}
+
+	threshold, err := strconv.Atoi(os.Getenv("THRESHOLD"))
+	if err != nil {
+		log.Printf("error parsing threshold value: %s\nfalling back to default value: 10\n", err)
+		config.Threshold = 10
+	} else {
+		config.Threshold = threshold
+	}
+
+	telegramID, err := strconv.Atoi(os.Getenv("TELEGRAM_ID"))
+	if err != nil {
+		log.Fatalf("parsing telegramID failed : %s\n", err)
+	} else {
+		config.TelegramID = telegramID
+	}
+
+	config.TelegramBotToken = strings.TrimSpace(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if config.TelegramBotToken == "" {
+		log.Fatal("telegram bot token cannot be empty")
+	}
 }
