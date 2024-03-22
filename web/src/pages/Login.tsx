d686f5cd370b3cbd1d5411a4ca1506a853a8d520
Rajkumar <rajkumaar2304@icloud.com>
2024-03-21 22:53:18 -0700

    add router and login page

---
+++ web/src/pages/Login.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Login() {
+  return (
+    <div className="container mt-32">
+      <Card className="md:w-2/4 m-auto border-none">
+        <CardHeader>
+          <CardTitle className="text-2xl lg:text-6xl">tls watch <span className="text-xs lg:text-xl">as a service</span></CardTitle>
+          <CardDescription className="lg:text-xl">
+            monitor your tls certificates without hassle
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button className="m-auto lg:text-xl">login to start &#8594;</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
