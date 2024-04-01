80fea43d27996a33bf30e4a74b976dcb66df4eaa
Rajkumar <rajkumaar2304@icloud.com>
2024-03-31 22:42:18 -0700

    update brand in nav

--- web/src/components/nav.tsx
+++ web/src/components/nav.tsx
@@ -6,7 +6,10 @@ export function MainNav() {
     <div className="flex gap-6 md:gap-10">
       <Link to="/" className="items-center space-x-2 flex">
         <Icons.watch />
-        <span className="font-bold sm:inline-block">tls watch</span>
+        <span className="font-bold sm:inline-block text-lg">
+          tls watch
+          <span className="text-xs ml-1 text-muted-foreground space-x-1 hidden md:inline-block">as a service</span>
+        </span>
       </Link>
     </div>
   );
